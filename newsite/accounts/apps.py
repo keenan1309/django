691a426060ef1067fb46e654fe8abea0ccf52911
Keenan <iprkeenan3112@gmail.com>
2023-09-10 12:54:35 +0200

    new app version

--- newsite/accounts/apps.py
+++ newsite/accounts/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """AppConfig
+
+    Args:
+        AppConfig (_type_): _description_
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
